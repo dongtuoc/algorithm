d06edaa72e8152c7d7aee728cdd2b25621b54441
dongtuoc <39508507+dongtuoc@users.noreply.github.com>
2019-03-30 10:43:51 +0800

    Update snake.c

--- snake.c
+++ snake.c
@@ -180,347 +180,334 @@ static void ShowInitialSnake(void)
 
 void ShowCityWall(void)
 {
-	U8 linetemp=0;
-	U8 numtemp=0;
-   	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+   U8 linetemp=0;
+   U8 numtemp=0;
+   HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	for(linetemp=1;linetemp<=SumLine;linetemp++)
-	{
-		for(numtemp=1;numtemp<=SumNum;numtemp++)
-		{
-			if ((numtemp==1)||(numtemp==SumNum)||(linetemp==1)||(linetemp==SumLine))
-			{
-				SetConsoleTextAttribute(hOut, Red);
-				gotoxy(linetemp,numtemp);
-				printf("¡õ");
-				SetDotType(linetemp,numtemp,DotWall);
-			}
-			else
-			{
-				SetConsoleTextAttribute(hOut, Green);
-				gotoxy(linetemp,numtemp);
-				printf("¡ö");
-				SetDotType(linetemp,numtemp,DotIdle);
-			}
-		}
-		printf("\r\n");
-	}
+   for(linetemp=1;linetemp<=SumLine;linetemp++)
+   {
+      for(numtemp=1;numtemp<=SumNum;numtemp++)
+      {
+         if ((numtemp==1)||(numtemp==SumNum)||(linetemp==1)||(linetemp==SumLine))
+         {
+            SetConsoleTextAttribute(hOut, Red);
+            gotoxy(linetemp,numtemp);
+            printf("¡õ");
+            SetDotType(linetemp,numtemp,DotWall);
+         }
+         else
+         {
+            SetConsoleTextAttribute(hOut, Green);
+            gotoxy(linetemp,numtemp);
+            printf("¡ö");
+            SetDotType(linetemp,numtemp,DotIdle);
+         }
+      }
+      printf("\r\n");
+   }
 }
 
 /* food eaten, trans food to head */
 void Food2Head(U8 foodline,U8 foodnum)
 {
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	NewHead(foodline,foodnum);
-	gotoxy(foodline, foodnum);
-	SetConsoleTextAttribute(hOut, Yellow);	
-	printf("¡ñ");
-	SetDotType(foodline,foodnum,DotHead);
-	Snakelen++;
+  NewHead(foodline,foodnum);
+  gotoxy(foodline, foodnum);
+  SetConsoleTextAttribute(hOut, Yellow);	
+  printf("¡ñ");
+  SetDotType(foodline,foodnum,DotHead);
+  Snakelen++;
 }
 
 /* food eaten, trans head to body */
 void Head2Body(U8 headline,U8 headnum)
 {
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	NewBody(headline,headnum);
-	gotoxy(headline, headnum);
-	SetConsoleTextAttribute(hOut, Yellow);	
-	printf("¡ð");
-	SetDotType(headline,headnum,DotBody);
-	Bodylen++;
+  NewBody(headline,headnum);
+  gotoxy(headline, headnum);
+  SetConsoleTextAttribute(hOut, Yellow);	
+  printf("¡ð");
+  SetDotType(headline,headnum,DotBody);
+  Bodylen++;
 }
 
 void IdeHead2Body(U8 headline,U8 headnum)
 {
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	gotoxy(headline, headnum);
-	SetConsoleTextAttribute(hOut, Yellow);	
-	printf("¡ð");
-	SetDotType(headline,headnum,DotBody);
+  gotoxy(headline, headnum);
+  SetConsoleTextAttribute(hOut, Yellow);	
+  printf("¡ð");
+  SetDotType(headline,headnum,DotBody);
 }
 
 /* idle eaten, trans idle to head */
 void Idle2Head(U8 Idleline,U8 Idlenum)
 {
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	NewHead(Idleline,Idlenum);
-	gotoxy(Idleline,Idlenum);
-	SetConsoleTextAttribute(hOut, Yellow);	
-	printf("¡ñ");	
-	SetDotType(Idleline,Idlenum,DotHead);
+  HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  NewHead(Idleline,Idlenum);
+  gotoxy(Idleline,Idlenum);
+  SetConsoleTextAttribute(hOut, Yellow);	
+  printf("¡ñ");	
+  SetDotType(Idleline,Idlenum,DotHead);
 }
 
 /* trans dot to idle */
 void Dot2Idle(U8 line,U8 num)
 {
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	gotoxy(line, num);
-	SetConsoleTextAttribute(hOut, Green);
-	printf("¡ö");
-	SetDotType(line,num,DotIdle);
+  gotoxy(line, num);
+  SetConsoleTextAttribute(hOut, Green);
+  printf("¡ö");
+  SetDotType(line,num,DotIdle);
 }
 
 /* idle eaten, update body */
 void IdleUpdateBody(void)
 {
-	U8 i;
-
-	if (0 == Bodylen)
-		return;
+  U8 i;
 	
-	for(i=0;i<Bodylen-1;i++)
-	{
-		Body[i].line = Body[i+1].line;
-		Body[i].num = Body[i+1].num;
-		SetDotType(Body[i].line,Body[i].num,DotBody);
-	}
+  if (0 == Bodylen) return;
+	
+  for(i=0;i<Bodylen-1;i++)
+  {
+    Body[i].line = Body[i+1].line;
+    Body[i].num = Body[i+1].num;
+    SetDotType(Body[i].line,Body[i].num,DotBody);
+  }
 
-	Body[Bodylen-1].line = Head.line;
-	Body[Bodylen-1].num = Head.num;
-	SetDotType(Body[Bodylen-1].line,Body[Bodylen-1].num,DotBody);
+  Body[Bodylen-1].line = Head.line;
+  Body[Bodylen-1].num = Head.num;
+  SetDotType(Body[Bodylen-1].line,Body[Bodylen-1].num,DotBody);
 }
 
 U8 ShowGameOver(void)
 {
-	U8 key;
+  U8 key;
 
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hOut, (Yellow<<4)|White);
-	gotoxy(SumLine/2,SumNum/2 - 6);
-	printf("  YOU LOST! PRESS:          ");
-	SetConsoleTextAttribute(hOut, (Yellow<<4)|White);
-	gotoxy(SumLine/2+1,SumNum/2 - 6);
-	printf("  ESC -- exit               ");
-	SetConsoleTextAttribute(hOut, (Yellow<<4)|White);
-	gotoxy(SumLine/2+2,SumNum/2 - 6);
-	printf("  Other Key -- Continue     ");
+  HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleTextAttribute(hOut, (Yellow<<4)|White);
+  gotoxy(SumLine/2,SumNum/2 - 6);
+  printf("  YOU LOST! PRESS:          ");
+  SetConsoleTextAttribute(hOut, (Yellow<<4)|White);
+  gotoxy(SumLine/2+1,SumNum/2 - 6);
+  printf("  ESC -- exit               ");
+  SetConsoleTextAttribute(hOut, (Yellow<<4)|White);
+  gotoxy(SumLine/2+2,SumNum/2 - 6);
+  printf("  Other Key -- Continue     ");
 
-	gotoxy(SumLine+2,SumNum/2-3+6);
-	key = GetInput();
-	if (key == ESC)
-		return 1;
-	else
-		return 2;
+  gotoxy(SumLine+2,SumNum/2-3+6);
+  key = GetInput();
+  if (key == ESC)
+    return 1;
+  else
+    return 2;
 }
 
 U8 mainloop(U8 key)
 {
-	U8 i=0;
-	
-	switch(key)
-	{
-		case Right:
-			switch(GetDotType(Head.line,Head.num+1))
-			{
-				case DotWall:
-				case DotBody:
-					return YouAreDone;
-				case DotFood:
-					Head2Body(Head.line,Head.num);
-					Food2Head(Food.line,Food.num);
-					
-					FoodIsThereFlag = ifalse;
-					Score += ScoreStep;
-					return YouAreExcllent;
-				case DotIdle:
-					IdeHead2Body(Head.line,Head.num);
-					if (1 == Snakelen)
-					{
-						Dot2Idle(Head.line,Head.num);
-					}
-					else
-					{
-						Dot2Idle(Body[0].line,Body[0].num);
-					}
+  U8 i=0;
 
-					IdleUpdateBody();
-					Idle2Head(Head.line,Head.num+1);				
-					return YouAreStillYou;
-				default:
-					return WhoYouAre;	
-			}
-			break;
-		case Down:
-			switch(GetDotType(Head.line+1,Head.num))
-			{
-				case DotWall:					
-				case DotBody:
-					return YouAreDone;
-				case DotFood:
-					Head2Body(Head.line,Head.num);
-					Food2Head(Food.line,Food.num);
-					FoodIsThereFlag = ifalse;
-					Score += ScoreStep;
-					return YouAreExcllent;
-				case DotIdle:
-					IdeHead2Body(Head.line,Head.num);
-					if (1 == Snakelen)
-					{
-						Dot2Idle(Head.line,Head.num);
-					}
-					else
-					{
-						Dot2Idle(Body[0].line,Body[0].num);
-					}
-					IdleUpdateBody();
-					Idle2Head(Head.line+1,Head.num);		
-					return YouAreStillYou;
-				default:
-					return WhoYouAre;	
-			}
-			break;			
-		case Up:
-			switch(GetDotType(Head.line-1,Head.num))
-			{
-				case DotWall:
-				case DotBody:
-					return YouAreDone;
-				case DotFood:
-					Head2Body(Head.line,Head.num);
-					Food2Head(Food.line,Food.num);
-					FoodIsThereFlag = ifalse;
-					Score += ScoreStep;
-					return YouAreExcllent;
-				case DotIdle:
-					IdeHead2Body(Head.line,Head.num);
-					if (1 == Snakelen)
-					{
-						Dot2Idle(Head.line,Head.num);
-					}
-					else
-					{
-						Dot2Idle(Body[0].line,Body[0].num);
-					}
-					IdleUpdateBody();
-					Idle2Head(Head.line-1,Head.num);				
-					return YouAreStillYou;
-				default:
-					return WhoYouAre;	
-			}
-			break;	
-		case Left:
-			switch(GetDotType(Head.line,Head.num-1))
-			{
-				case DotWall:
-				case DotBody:
-					return YouAreDone;
-				case DotFood:
-					Head2Body(Head.line,Head.num);
-					Food2Head(Food.line,Food.num);
-					FoodIsThereFlag = ifalse;
-					Score += ScoreStep;
-					return YouAreExcllent;
-				case DotIdle:
-					IdeHead2Body(Head.line,Head.num);
-					if (1 == Snakelen)
-					{
-						Dot2Idle(Head.line,Head.num);
-					}
-					else
-					{
-						Dot2Idle(Body[0].line,Body[0].num);
-					}
-					IdleUpdateBody();
-					Idle2Head(Head.line,Head.num-1);				
-					return YouAreStillYou;
-				default:
-					return WhoYouAre; 
-			}
-			break;	
-		default:
-			return WhoYouAre;
-	}
+  switch(key)
+  {
+  case Right:
+    switch(GetDotType(Head.line,Head.num+1))
+    {
+    case DotWall:
+    case DotBody:
+      return YouAreDone;
+    case DotFood:
+      Head2Body(Head.line,Head.num);
+      Food2Head(Food.line,Food.num);
+
+      FoodIsThereFlag = ifalse;
+      Score += ScoreStep;
+      return YouAreExcllent;
+    case DotIdle:
+      IdeHead2Body(Head.line,Head.num);
+      if (1 == Snakelen)
+         Dot2Idle(Head.line,Head.num);
+      else
+         Dot2Idle(Body[0].line,Body[0].num);
+
+      IdleUpdateBody();
+      Idle2Head(Head.line,Head.num+1);				
+      return YouAreStillYou;
+    default:
+      return WhoYouAre;	
+    }
+    break;
+  case Down:
+    switch(GetDotType(Head.line+1,Head.num))
+    {
+    case DotWall:					
+    case DotBody:
+      return YouAreDone;
+    case DotFood:
+      Head2Body(Head.line,Head.num);
+      Food2Head(Food.line,Food.num);
+      FoodIsThereFlag = ifalse;
+      Score += ScoreStep;
+      return YouAreExcllent;
+    case DotIdle:
+      IdeHead2Body(Head.line,Head.num);
+      if (1 == Snakelen)
+        Dot2Idle(Head.line,Head.num);
+      else
+        Dot2Idle(Body[0].line,Body[0].num);
+      
+      IdleUpdateBody();
+      Idle2Head(Head.line+1,Head.num);		
+      return YouAreStillYou;
+    default:
+      return WhoYouAre;	
+    }
+    break;			
+  case Up:
+    switch(GetDotType(Head.line-1,Head.num))
+    {
+    case DotWall:
+    case DotBody:
+      return YouAreDone;
+    case DotFood:
+      Head2Body(Head.line,Head.num);
+      Food2Head(Food.line,Food.num);
+      FoodIsThereFlag = ifalse;
+      Score += ScoreStep;
+      return YouAreExcllent;
+    case DotIdle:
+      IdeHead2Body(Head.line,Head.num);
+      if (1 == Snakelen)
+        Dot2Idle(Head.line,Head.num);
+      else
+        Dot2Idle(Body[0].line,Body[0].num);
+
+      IdleUpdateBody();
+      Idle2Head(Head.line-1,Head.num);				
+      return YouAreStillYou;
+    default:
+      return WhoYouAre;	
+    }
+    break;	
+  case Left:
+    switch(GetDotType(Head.line,Head.num-1))
+    {
+      case DotWall:
+      case DotBody:
+        return YouAreDone;
+      case DotFood:
+        Head2Body(Head.line,Head.num);
+        Food2Head(Food.line,Food.num);
+        FoodIsThereFlag = ifalse;
+        Score += ScoreStep;
+        return YouAreExcllent;
+      case DotIdle:
+        IdeHead2Body(Head.line,Head.num);
+        if (1 == Snakelen)
+          Dot2Idle(Head.line,Head.num);
+        else
+          Dot2Idle(Body[0].line,Body[0].num);
+
+        IdleUpdateBody();
+        Idle2Head(Head.line,Head.num-1);				
+        return YouAreStillYou;
+      default:
+        return WhoYouAre; 
+      }
+      break;	
+    default:
+      return WhoYouAre;
+    }
 }
 
 int main(void)
 {
-	U8 key=0;
-	U8 res=0;
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+   U8 key=0;
+   U8 res=0;
+   HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	loop:for(;;)
-	{
-		AllInitail();
-		ShowCityWall();
+   loop:for(;;)
+   {
+      AllInitail();
+      ShowCityWall();
 
-		SetConsoleTextAttribute(hOut, Red);
-		gotoxy(SumLine+2,2);
-		printf("Author: Tuocan Dong");
-		gotoxy(SumLine+3,2);
-		printf("For more information:");
-		gotoxy(SumLine+5,2);
-		printf("http://www.alphadamo.com");
-		gotoxy(SumLine+4,2);
-		printf("Email:dongtuoc@yeah.net");
+      SetConsoleTextAttribute(hOut, Red);
+      gotoxy(SumLine+2,2);
+      printf("Author: Tuocan Dong");
+      gotoxy(SumLine+3,2);
+      printf("For more information:");
+      gotoxy(SumLine+5,2);
+      printf("http://www.alphadamo.com");
+      gotoxy(SumLine+4,2);
+      printf("Email:dongtuoc@yeah.net");
 
-		if (FirstRun == itrue)
-		{
-			SetConsoleTextAttribute(hOut, Red);
-			gotoxy(SumLine+2,16);
-			printf("PRESS ENTER TO START");
+      if (FirstRun == itrue)
+      {
+         SetConsoleTextAttribute(hOut, Red);
+         gotoxy(SumLine+2,16);
+         printf("PRESS ENTER TO START");
 
-			for(;;)
-			{
-				key = GetInput();
-				if ((key==Enter)||(key==Huanhang))
-					break;
-			}
-		}
-		gotoxy(SumLine+2,16);
-		printf("                    ");
-		SetConsoleTextAttribute(hOut, Red);
-		gotoxy(SumLine+2,16);
-		printf("GAME RUNNING");
-		
-		ShowInitialSnake();
+         for(;;)
+         {
+            key = GetInput();
+            if ((key==Enter)||(key==Huanhang))
+               break;
+         }
+      }
+   
+      gotoxy(SumLine+2,16);
+      printf("                    ");
+      SetConsoleTextAttribute(hOut, Red);
+      gotoxy(SumLine+2,16);
+      printf("GAME RUNNING");
 
-		SetConsoleTextAttribute(hOut, Red);
-		gotoxy(SumLine+3,16);
-		printf("Your Score:");
-		gotoxy(SumLine+4,16);
-		printf("Your  Goal:");
-		SetConsoleTextAttribute(hOut, Yellow);
-		gotoxy(SumLine+4,16 +6);
-		printf("%4d",(SumLine*SumNum*ScoreStep-1500));
-		
-		for(;;)
-		{
-			SetConsoleTextAttribute(hOut, Yellow);
-			gotoxy(SumLine+3,16+6);
-			printf("%04d",Score);
-		
-			if (FoodIsThereFlag == ifalse)
-				ShowRandomFood();
+      ShowInitialSnake();
 
-			gotoxy(SumLine+2,16+6);
-			key = GetInput();
+      SetConsoleTextAttribute(hOut, Red);
+      gotoxy(SumLine+3,16);
+      printf("Your Score:");
+      gotoxy(SumLine+4,16);
+      printf("Your  Goal:");
+      SetConsoleTextAttribute(hOut, Yellow);
+      gotoxy(SumLine+4,16 +6);
+      printf("%4d",(SumLine*SumNum*ScoreStep-1500));
 
-			res = mainloop(key);
-			switch(res)
-			{
-				case YouAreDone:
-					key = ShowGameOver();
-					if (key == 1)//esc
-					{
-						return 0;
-					}
-					else if (key == 2)
-					{
-						FirstRun = ifalse;
-						goto loop;
-					}
-					break;
-				case YouAreExcllent:
-				case YouAreStillYou:
-				case WhoYouAre:
-				default:
-					break;
-			}
-		}
-	}
+      for(;;)
+      {
+         SetConsoleTextAttribute(hOut, Yellow);
+         gotoxy(SumLine+3,16+6);
+         printf("%04d",Score);
+ 
+         if (FoodIsThereFlag == ifalse)
+            ShowRandomFood();
+
+         gotoxy(SumLine+2,16+6);
+         key = GetInput();
+
+         res = mainloop(key);
+         switch(res)
+         {
+            case YouAreDone:
+               key = ShowGameOver();
+               if (key == 1)//esc
+               {
+                  return 0;
+               }
+               else if (key == 2)
+               {
+                  FirstRun = ifalse;
+                  goto loop;
+               }
+               break;
+            case YouAreExcllent:
+            case YouAreStillYou:
+            case WhoYouAre:
+            default:
+               break;
+         }
+      }
+   }
 }
